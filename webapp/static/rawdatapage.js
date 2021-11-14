31c73fdfef658706ca19209f6e3bf50eee5e4fa4
millerr2024 <millerr2@carleton.edu>
2021-11-14 13:13:50 -0600

    raw data page fully functional

--- webapp/static/rawdatapage.js
+++ webapp/static/rawdatapage.js
@@ -5,6 +5,13 @@ window.onload = initialize;
 function initialize() {
 	let nameSortingButton = document.getElementById('name');
 	nameSortingButton.onclick = onNameSortingButtonClicked;
+	let ageSortingButton = document.getElementById('age');
+	ageSortingButton.onclick = onAgeSortingButtonClicked;
+	let passengerIDSortingButton = document.getElementById('passenger');
+	passengerIDSortingButton.onclick = onPassengerIDSortingButtonClicked;
+	let classSortingButton = document.getElementById('class');
+	classSortingButton.onclick = onClassSortingButtonClicked;
+
 }
 
 // Returns the base URL of the API, onto which endpoint components can be appended.
@@ -14,5 +21,45 @@ function getAPIBaseURL() {
 }
 
 function onNameSortingButtonClicked() {
-	
+	var url = getAPIBaseURL() + '/all/?sort=name';
+	formattingOutput(url);
 }
+
+function onAgeSortingButtonClicked() {
+	var url = getAPIBaseURL() + '/all/?sort=age';
+        formattingOutput(url);
+}
+
+function onPassengerIDSortingButtonClicked(){
+	var url = getAPIBaseURL() + '/all/';
+        formattingOutput(url);
+}
+
+function onClassSortingButtonClicked() {
+	var url = getAPIBaseURL() + '/all/?sort=class';
+        formattingOutput(url);
+}
+
+
+function formattingOutput(url) {
+	fetch(url, {method: 'get'})
+	.then((response) => response.json())
+        .then(function(everythingList) {
+		var tableBody = '';
+		tableBody += '<tr><th>ID</th><th>Survived</th><th>Class</th><th>Name</th><th>Sex</th><th>Age</th><th>Sibsp</th><th>Parch</th><th>Ticket</th><th>Fare</th><th>Cabin</th><th>Embarked</th>';
+		for (var k = 0; k < everythingList.length; k++) {
+			tableBody += '<tr>';
+			tableBody += '<td>' + everythingList[k]['id'] + '</td>' + '<td>' + everythingList[k]['survived'] + '</td>' + '<td>' + everythingList[k]['class'] + '</td>' + '<td>' + everythingList[k]['name'] + '</td>' + '<td>' + everythingList[k]['sex'] + '</td>'+ '<td>' + everythingList[k]['age'] + '</td>' + '<td>' + everythingList[k]['sibsp'] + '</td>'+ '<td>' + everythingList[k]['parch'] + '</td>' + '<td>' + everythingList[k]['ticket'] + '</td>' + '<td>' + everythingList[k]['fare'] + '</td>' + '<td>' + everythingList[k]['cabin'] + '</td>' + '<td>' + everythingList[k]['embarked'] + '</td>';
+			tableBody += '</tr>';
+		}
+		var resultsTableElement = document.getElementById('results_table');
+		if (resultsTableElement) {
+			resultsTableElement.innerHTML = tableBody;
+		}
+	})
+		.catch(function(error) {
+			console.log(error);
+	});
+}
+
+
